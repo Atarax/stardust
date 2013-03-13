17cba22bd54276745fc9f743613da6e6615ee371
Tobias Kässer <tk@plista.com>
2013-03-13 15:52:48 +0100

    db-bugfix

--- classes/Item.php
+++ classes/Item.php
@@ -11,7 +11,7 @@ class Item {
 	private $config = array(
 		"mysql_host" => "localhost",
 		"mysql_user" => "root",
-		"mysql_pass" => "yfmaster00pw"
+		"mysql_pass" => "g9H43b"
 	);
 
 	public $id;
@@ -22,7 +22,6 @@ class Item {
 		/**
 		 * @var $config defined in config.php
 		 */
-		if(!isset($this->id))
 		mysql_connect( $this->config["mysql_host"], $this->config["mysql_user"], $this->config["mysql_pass"] );
 		mysql_query( "
 				INSERT INTO contest.item (id, domain, recommendable)
--- tmp.php
+++ tmp.php
@@ -2,8 +2,8 @@
 
 require_once("config.php");
 $item = new Item();
-$item->id = 0;
-$item->domain = 0;
-$item->recommendable = 0;
+$item->id = 123;
+$item->domain = 456;
+$item->recommendable = 7;
 
 $item->save();
