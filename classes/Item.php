330fa0f0afafbbafe2dbf82d4b20ac288ada62d4
Tobias Kässer <tk@plista.com>
2013-03-13 15:17:57 +0100

    bugfix

--- classes/Item.php
+++ classes/Item.php
@@ -6,12 +6,13 @@
  * Time: 2:42 PM
  * To change this template use File | Settings | File Templates.
  */
-$config = array();
-$config["mysql_host"] = "localhost";
-$config["mysql_user"] = "root";
-$config["mysql_pass"] = "g9H43b";
 
 class Item {
+	$config = array();
+	$config["mysql_host"] = "localhost";
+	$config["mysql_user"] = "root";
+	$config["mysql_pass"] = "g9H43b";
+
 	public $id;
 	public $domain;
 	public $recommendable;
@@ -20,7 +21,7 @@ class Item {
 		/**
 		 * @var $config defined in config.php
 		 */
-		mysql_connect( $config["mysql_host"], $config["mysql_user"], $config["mysql_pass"] );
+		mysql_connect( $this->config["mysql_host"], $this->config["mysql_user"], $this->config["mysql_pass"] );
 		mysql_query( "INSERT INTO contest.item (id, domain, recommendable) VALUES (".$this->id.",". $this->domain.",". $this->recommendable.") ON DUPLICATE KEY UPDATE" );
 		mysql_close();
 	}
