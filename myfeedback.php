6d585408a8418a700c4e973adcb4c1391b820d99
Tobias Kässer <tk@plista.com>
2013-03-15 12:16:15 +0100

    view changes

--- myfeedback.php
+++ myfeedback.php
@@ -10,7 +10,7 @@ require_once("config.php");
 
 if( isset($_GET["ajax"]) ) {
 	$db = new DatabaseManager();
-	$res = $db->query("SELECT * FROM contest.feedback WHERE team = 227 ORDER BY CREATED DESC");
+	$res = $db->query("SELECT * FROM contest.feedback WHERE team = 227");
 	die( json_encode( array("data" => $res ) ) );
 }
 
