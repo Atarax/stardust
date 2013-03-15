39a3017959c916923957afaff2b0ddee55b7477a
Tobias Kässer <tk@plista.com>
2013-03-15 19:41:32 +0100

    cronjob monitoring

--- classes/HottestItemScore.php
+++ classes/HottestItemScore.php
@@ -6,7 +6,7 @@
  * Time: 7:21 PM
  * To change this template use File | Settings | File Templates.
  */
-class Hottestitemscore extends StardustModel {
+class HottestItemScore extends StardustModel {
 	public $item;
 	public $score;
 }
