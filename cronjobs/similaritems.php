ebef86a636bff02afdc5d1b828d6f1a3a12952e3
Tobias Kässer <tk@plista.com>
2013-03-17 23:18:59 +0100

    working to new recommender

--- cronjobs/similaritems.php
+++ cronjobs/similaritems.php
@@ -8,16 +8,15 @@ mb_internal_encoding('UTF-8');
 $db = DatabaseManager::getInstace();
 $db->connect();
 $items = $db->query("
-				SELECT id FROM contest.item;
+				SELECT id, domain FROM contest.item;
 			");
 
 $db->query("TRUNCATE TABLE contest.similaritems");
 
 foreach( $items as $i => $item ) {
 	$relateditems = $db->query("
-		SELECT i2.item AS id, i1.count*i2.count AS similarity FROM contest.itembuzzword i1, contest.itembuzzword i2 WHERE i1.item = ".$item["id"]." AND i1.buzzword = i2.buzzword AND i1.item != i2.item ORDER BY similarity DESC LIMIT 20;
+		SELECT i2.item AS id, i1.count*i2.count AS similarity FROM contest.item i, contest.itembuzzword i1, contest.itembuzzword i2 WHERE i.id = i2.item AND i.domain = ".$item["domain"]." AND i1.item = ".$item["id"]." AND i1.buzzword = i2.buzzword AND i1.item != i2.item ORDER BY similarity DESC LIMIT 20;
 	");
-
 	echo "Item $i (".$item["id"].") of ".count($items)."\n";
 
 	$query = "INSERT INTO contest.similaritems(item, similaritem, similarity) VALUES ";
