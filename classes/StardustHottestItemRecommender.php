e1c193e3ff5af36d1398919d6da8c80b75015422
Tobias Kässer <tk@plista.com>
2013-03-24 11:21:32 +0100

    bugfixing (timeouts)

--- classes/StardustHottestItemRecommender.php
+++ classes/StardustHottestItemRecommender.php
@@ -37,7 +37,7 @@ class StardustHottestItemRecommender implements ContestRecommender {
 			$data = $db->query($query);
 			$duration = microtime(true) - $t1;
 
-			file_put_contents("log/hottestexecutiontime", date('c') . " Execution Time: ".sprintf('%.3f', $duration)."\n".$query."\n", FILE_APPEND);
+			file_put_contents("log/hottestexecutiontime", date('c') . " Execution Time: ".sprintf('%.3f', $duration)."\n", FILE_APPEND);
 
 			shuffle($data);
 
