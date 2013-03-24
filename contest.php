0c89cb41be24282c241be8aa1373ca3c8069541e
Tobias Kässer <tk@plista.com>
2013-03-24 10:39:04 +0100

    bugfixing (timeouts)

--- contest.php
+++ contest.php
@@ -79,5 +79,5 @@ try {
 
 $t2 = microtime(true);
 
-file_put_contents("log/executiontime", date('c') . " Execution Time: ".print_r( $t2-t1, true)."\n".print_r($msg, true), FILE_APPEND);
+file_put_contents("log/executiontime", date('c') . " Execution Time: ".( $t2-t1)."\n".print_r($msg, true), FILE_APPEND);
 
