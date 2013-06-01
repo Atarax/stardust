c6bb108c660eae58e344eb86338c37e9e9f86f06
Tobias Kässer <tk@plista.com>
2013-06-01 23:46:25 +0200

    small improvement ^^

--- classes/StardustShanonRecommender.php
+++ classes/StardustShanonRecommender.php
@@ -52,6 +52,7 @@ class StardustShanonRecommender implements ContestRecommender {
 				itembuzzword.buzzword IN (".implode(",", $tmp ).") AND
 				item.id != ".$contestImpression->item->id." AND
 				item.domain = ".$domainid." AND
+				item.title != ".preg_replace('/[^\P{C}\n]+/u', '', $contestImpression->item->title)." AND
 				item.recommendable > 0
 				".$filter."
 			GROUP BY
